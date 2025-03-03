b1b051b76dbbb2a15c7bc7bdd4e0d7fde7c47df5
IssamElJaouhari <issamefar58@gmail.com>
2025-03-03 14:08:44 +0000

    refactor: remove old MongoDB test file and update package.json with nodemon

--- backend/testMongo.js
+++
@@ -1,8 +0,0 @@
-//testMongo.js
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected successfully'))
-  .catch((err) => console.log('Error:', err));
